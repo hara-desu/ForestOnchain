8a9f48d1605576e3013e25f6e5367c2385588d6d
hara-desu <noname3564@proton.me>
2025-11-11 12:20:58 +0300

    Added chainlink automation

--- src/ForestOnchain.sol
+++ src/ForestOnchain.sol
@@ -4,15 +4,20 @@
 // 2. Add tree types
 pragma solidity ^0.8.13;
 
+import {AutomationCompatibleInterface} from "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
+
 contract ForestOnchain {
     error ForestOnchain__AddAnActivityType();
     error ForestOnchain_AnotherSessionOngoing(string currentSession);
-    error ForestOnchain_NoOngoingSession(msg.sender);
+    error ForestOnchain_NoOngoingSession(address user);
+    error ForestOnchain_BreakNeeded();
 
     mapping(address => string[]) public userActivityTypes;
     mapping(address => UserSession) public currentUserSession;
     mapping(address => uint256) public numberOfTrees;
     mapping(address => bool) public breakNeeded;
+    address[] public users;
+    mapping(address => uint256) public activeUserIdx;
 
     event ActivityAdded(string indexed activityType);
     event SessionStarted(
@@ -48,7 +53,9 @@ contract ForestOnchain {
                 currentUserSession[msg.sender].activityType
             );
         }
-        // ADD BREAK CHECK
+        if (breakNeeded[msg.sender]) {
+            revert ForestOnchain_BreakNeeded();
+        }
         currentUserSession[msg.sender] = UserSession({
             activityType: _activityType,
             startTime: block.timestamp,
@@ -58,22 +65,44 @@ contract ForestOnchain {
         emit SessionStarted(
             msg.sender,
             _activityType,
-            currentUserSession.startTime,
-            currentUserSession.endTime
+            currentUserSession[msg.sender].startTime,
+            currentUserSession[msg.sender].endTime
         );
     }
 
-    function endFocusSession() public {
-        bool sessionActive = currentUserSession[msg.sender].active;
-        if (!sessionActive) {
-            revert ForestOnchain_NoOngoingSession(msg.sender);
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    ) external view returns (bool upkeepNeeded, bytes memory performData) {
+        uint256 numberUsers = users.length;
+        if (numberUsers == 0) {
+            return (false, bytes(""));
         }
-        if (currentUserSession[msg.sender].endTime > block.timestamp) {
-            delete currentCampaign;
+        for (uint i = 0; i < numberUsers; i++) {
+            address user = users[i];
+            UserSession memory session = currentUserSession[user];
+            if (session.active && block.timestamp >= session.endTime) {
+                return (true, abi.encode(user));
+            }
+        }
+        return (false, bytes(""));
+    }
+
+    function performUpkeep(bytes calldata performData) external {
+        address user = abi.decode(performData, (address));
+        endFocusSession(user);
+    }
+
+    function endFocusSession(address _user) public {
+        bool sessionActive = currentUserSession[_user].active;
+        if (!sessionActive) {
+            revert ForestOnchain_NoOngoingSession(_user);
+        }
+        if (currentUserSession[_user].endTime > block.timestamp) {
+            delete currentUserSession[_user];
         } else {
-            numberOfTrees += 1;
-            delete currentCampaign;
-            breakNeeded[msg.sender] = true;
+            numberOfTrees[_user] += 1;
+            delete currentUserSession[_user];
+            breakNeeded[_user] = true;
         }
     }
 }
